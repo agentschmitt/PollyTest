c6b921c3f9ce73e7027163b0630ad1f5227db148
Roland Schmitt <rschmitt@samhammer.de>
2021-07-12 11:29:49 +0200

    change polly to async

--- PollyApp/Program.cs
+++ PollyApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Polly;
 
 namespace PollyApp
@@ -6,30 +7,32 @@ namespace PollyApp
     static class Program
     {
         private static int _doCount;
+        private static int _failCount = 2;
 
-        static void Main()
+        static async Task Main()
         {
             var fallbackValue = "FALLBACK";
 
             var fallback = Policy<string>
                 .Handle<Exception>()
-                .Fallback(fallbackValue, (result) =>
+                .FallbackAsync(fallbackValue, result =>
                 {
                     Console.WriteLine($"polly fallback cause of {result.Exception.Message}");
+                    return Task.CompletedTask;
                 });
 
             var retry = Policy<string>
                 .Handle<Exception>()
-                .Retry(1, (result, _) =>
+                .RetryAsync(1, (result, _) =>
                 {
                     Console.WriteLine($"polly retry cause of {result.Exception.Message}");
                 });
 
-            var policy = Policy.Wrap(fallback, retry);
+            var policy = Policy.WrapAsync(fallback, retry);
 
             try
             {
-                var result = policy.Execute(DoSomething);
+                var result = await policy.ExecuteAsync(DoSomething);
                 Console.WriteLine($"polly result {result}");
             }
             catch (Exception ex)
@@ -40,13 +43,13 @@ namespace PollyApp
             Console.ReadLine();
         }
 
-        static string DoSomething()
+        static Task<string> DoSomething()
         {
             _doCount++;
             Console.WriteLine($"try {_doCount}");
 
             // fail on first run
-            if (_doCount < 3)
+            if (_doCount <= _failCount)
             {
                 Console.WriteLine($"try {_doCount} failing");
                 throw new Exception($"try {_doCount} exception");
@@ -54,7 +57,7 @@ namespace PollyApp
             else
             {
                 Console.WriteLine($"try {_doCount} success");
-                return "I DID IT";
+                return Task.FromResult("I DID IT");
             }
         }
     }
